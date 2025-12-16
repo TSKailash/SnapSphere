5c695ea4496e168f606787676bc40fdaf9d1260f
TSKailash <tskailash20@gmail.com>
2025-12-16 20:11:35 +0530

    Completed

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10000,
   message: "Too many requests. Try again later."
 });
